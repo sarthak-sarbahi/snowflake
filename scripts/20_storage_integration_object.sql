e4d7fa125a22a22936b8a1210500c90a298b4e44
sarthak-sarbahi <infernape@LAPTOP-436V44KT.localdomain>
2023-11-22 17:34:34 +0530

    completed till section 10

--- scripts/20_storage_integration_object.sql
+++ scripts/20_storage_integration_object.sql
@@ -25,3 +25,34 @@ CREATE OR REPLACE file format MANAGE_DB.file_formats.csv_fileformat
     null_if = ('NULL','null')
     empty_field_as_null = TRUE    
     FIELD_OPTIONALLY_ENCLOSED_BY = '"' -- if a value in a column has commas in it then the value can be enclosed with ""   
+
+-- Create storage integration object: Azure Storage Account -- 
+CREATE STORAGE INTEGRATION azure_integration
+  TYPE = EXTERNAL_STAGE
+  STORAGE_PROVIDER = AZURE
+  ENABLED = TRUE
+  AZURE_TENANT_ID = '' -- go to active directory to get tenant ID
+  STORAGE_ALLOWED_LOCATIONS = ('azure://<account_name>.blob.core.windows.net/<container_name>', 'azure://<account_name>.blob.core.windows.net/<container_name>'); -- storage account name
+
+-- Describe integration object to provide access
+-- Click on AZURE_CONSENT_URL and provide required approval
+-- Add role assigment to storage account (contributor), try to search for snowflake and a name should appear
+DESC STORAGE integration azure_integration;  
+
+-- create stage object
+create or replace stage demo_db.public.stage_azure
+    STORAGE_INTEGRATION = azure_integration
+    URL = 'azure://storageaccountsnow.blob.core.windows.net/snowflakecsv'
+    FILE_FORMAT = fileformat_azure;
+
+-- Create storage integration object: GCS --
+-- create integration object that contains the access information
+CREATE STORAGE INTEGRATION gcp_integration
+  TYPE = EXTERNAL_STAGE
+  STORAGE_PROVIDER = GCS
+  ENABLED = TRUE
+  STORAGE_ALLOWED_LOCATIONS = ('gcs://<bucket_name>/path', 'gcs://<bucket_name>/path2');
+
+-- Describe integration object to provide access
+-- add member to GCS buckets (STORAGE_GCP_SERVICE_ACCOUNT) and assign storage related role
+DESC STORAGE integration gcp_integration;
